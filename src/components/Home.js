9e82ad368c5ff4bc55edd981e6bc58562e854d08
falrabadi <fa979119@ohio.edu>
2022-08-30 01:22:42 -0400

    test

--- src/components/Home.js
+++ src/components/Home.js
@@ -10,7 +10,7 @@ function Home() {
             <Row>
                 <Col md={7} className="home-header">
                 <h1 style={{ paddingBottom: 15 }} className="heading">
-                    Welcome!{" "}
+                    Welcome! HELOOLDSOGSLDG{" "}
                     <span className="wave" role="img" aria-labelledby="wave">
                         😊
                     </span>
