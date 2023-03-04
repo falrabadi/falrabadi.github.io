45e201b16944cb5d6609afba3dc2336255bc80d3
falrabadi <fa979119@ohio.edu>
2023-03-04 16:01:41 -0500

    Trying to fix 404 error

--- src/App.js
+++ src/App.js
@@ -7,10 +7,10 @@ import Projects from "./components/Projects";
 import Resume from "./components/Resume";
 import Preloader from "./components/Pre";
 import {
-  BrowserRouter as Router,
-  Route,
-  Routes,
-  Navigate
+    BrowserRouter as Router,
+    Route,
+    Routes,
+    Navigate
 } from "react-router-dom";
 import ScrollToTop from "./components/ScrollToTop";
 import "./styles.css";
@@ -25,34 +25,46 @@ ReactGA.pageview(window.location.pathname + window.location.search);
 ReactGA.initialize(TRACKING_ID);
 
 function App() {
-  const [load, upadateLoad] = useState(true);
+    const [load, upadateLoad] = useState(true);
 
-  useEffect(() => {
-    const timer = setTimeout(() => {
-      upadateLoad(false);
-    }, 1200);
+    useEffect(() => {
+        const timer = setTimeout(() => {
+            upadateLoad(false);
+        }, 1200);
 
-    return () => clearTimeout(timer);
-  }, []);
+        return () => clearTimeout(timer);
+    }, []);
 
-  return (
-    <Router>
-        <Preloader load={load} />
-        <div className="App" id={load ? "no-scroll" : "scroll"}>
-            <Navbar />
-            <ScrollToTop />
-            <Particles />
-            <Routes>
-                <Route path="/" element={<Home />} />
-                <Route path="/project" element={<Projects />} />
-                <Route path="/about" element={<About />} />
-                <Route path="/resume" element={<Resume />} />
-                <Route path="*" element={<Navigate to="/"/>} />
-            </Routes>
-        </div>
-    </Router>
-    
-  );
+    if ('serviceWorker' in navigator) {
+        window.addEventListener('load', () => {
+            navigator.serviceWorker.register('/service-worker.js')
+                .then(registration => {
+                    console.log('Service worker registered:', registration);
+                })
+                .catch(error => {
+                    console.error('Service worker registration failed:', error);
+                });
+        });
+    }
+
+    return (
+        <Router>
+            <Preloader load={load} />
+            <div className="App" id={load ? "no-scroll" : "scroll"}>
+                <Navbar />
+                <ScrollToTop />
+                <Particles />
+                <Routes>
+                    <Route path="/" element={<Home />} />
+                    <Route path="/project" element={<Projects />} />
+                    <Route path="/about" element={<About />} />
+                    <Route path="/resume" element={<Resume />} />
+                    <Route path="*" element={<Navigate to="/" />} />
+                </Routes>
+            </div>
+        </Router>
+
+    );
 }
 
 export default App;
